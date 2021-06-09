7912841ca497ac7690df09cb20e2685465898f3a
Soori <soorichu@gmail.com>
2021-06-09 15:11:40 +0900

    before button

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -8,17 +8,15 @@ public class FollowCamera : MonoBehaviour
     public float dist = 7.0f;
     public float height = 4.0f;
     public float smoothRotate = 5.0f;
-    
+
     void LateUpdate()
     {
-        float currYAngle = Mathf.LerpAngle(transform.eulerAngles.y, 
-                                           target.eulerAngles.y, 
+        float currYAngle = Mathf.LerpAngle(transform.eulerAngles.y,
+                                           target.eulerAngles.y,
                                            smoothRotate * Time.deltaTime);
-
-        Quaternion rot = Quaternion.Euler(0, currYAngle, 0); 
-        transform.position = target.position 
-                            - (rot * Vector3.forward * dist) 
-                            + (Vector3.up * height); 
+        Quaternion rot = Quaternion.Euler(0, currYAngle, 0);
+        transform.position = target.position - (rot * Vector3.forward * dist) +
+                              (Vector3.up * height);
         transform.LookAt(target);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     private float translation;
     private float rotation;
+
+    private Animator ani;
+    private void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+
     void Update()
     {
         translation = Input.GetAxis("Vertical") * 10f;
@@ -13,5 +20,29 @@ public class PlayerController : MonoBehaviour
 
         transform.Translate(0, 0, translation * Time.deltaTime);
         transform.Rotate(0, rotation * Time.deltaTime, 0);
+
+        if (translation!=0 || rotation!=0)
+        {
+            ani.SetBool("walk", true);
+        }
+        else
+        {
+            ani.SetBool("walk", false);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Obstacle")
+        {
+            ani.SetTrigger("damage");
+            GameManager.instance.AddScore(-1);
+        }
+
+        if (collision.collider.tag == "Enermy")
+        {
+            ani.SetTrigger("attack01");
+            GameManager.instance.OnPlayerDead();
+        }
     }
 }
